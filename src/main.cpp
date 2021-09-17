92c8da2d309194866af69489f41df7309af6cd3c
CL <chencassc@gmail.com>
2021-09-17 15:17:44 +0800

    support 3 led strips

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #define PIN_LED_1 13
 #define PIN_LED_2 12
-#define PIN_LED_3 14 
+#define PIN_LED_3 14
 
 #define NUMPIXELS 8
 
@@ -12,31 +12,37 @@ Adafruit_NeoPixel led1(NUMPIXELS, PIN_LED_1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel led2(NUMPIXELS, PIN_LED_2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel led3(NUMPIXELS, PIN_LED_3, NEO_GRB + NEO_KHZ800);
 
+Adafruit_NeoPixel *leds[] = {&led1, &led2, &led3};
+
+auto R = Adafruit_NeoPixel::Color(10, 0, 0);
+auto G = Adafruit_NeoPixel::Color(0, 10, 0);
+auto B = Adafruit_NeoPixel::Color(0, 0, 10);
+
+uint32_t colors[] = {R, G, B};
 
 void setup()
 {
-  led1.begin();
-  led2.begin();
-  led3.begin();
+  for (auto led : leds)
+  {
+    led->begin();
+  }
 }
 
 void loop()
 {
-  led1.clear();
-  led2.clear();
-  led3.clear();
+  for (auto led : leds)
+  {
+    led->clear();
+  }
 
   for (int i = 0; i < NUMPIXELS; i++)
   {
-    led1.setPixelColor(i, led1.Color(0, 10, 0));
-    led1.show();
-
-    led2.setPixelColor(i, led2.Color(0, 0, 10));
-    led2.show();
-
-    led3.setPixelColor(i, led2.Color(10, 0, 0));
-    led3.show();
-
+    for (auto led : leds)
+    {
+      led->fill(0);
+      led->setPixelColor(i, colors[i%3]);
+      led->show();
+    };
     delay(50);
   }
 }
