41c1b967bcb634383307fc126402771774531939
Chen <chencassc@gmail.com>
2021-11-18 10:32:46 +0800

    merge FastLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,36 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include "main.h"
 
-// Configure the pins to control WS2812 LEDs
-#define PIN_LED_1 23
-#define PIN_LED_2 5
-#define PIN_LED_3 16
+#define PIN_LED_1 26
+#define PIN_LED_2 33
+#define PIN_LED_3 23
 
 // Number of LEDs on each WS2812 LED ring
 #define NUMPIXELS 24
 
-Adafruit_NeoPixel led1(NUMPIXELS, PIN_LED_1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel led2(NUMPIXELS, PIN_LED_2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel led3(NUMPIXELS, PIN_LED_3, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel *leds[] = {&led1, &led2, &led3};
+CRGB led1[NUMPIXELS];
+CRGB led2[NUMPIXELS];
+CRGB led3[NUMPIXELS];
+std::vector<CRGB *> leds;
+float updateLedSpeed[] = {.8, .8, .8};
+float currentBrightnessValue[] = {100.f, 100.f, 100.f};
+float finalBrightnessValue[] = {20.f, 20.f, 20.f};
 
-auto R = Adafruit_NeoPixel::Color(10, 0, 0);
-auto G = Adafruit_NeoPixel::Color(0, 10, 0);
-auto B = Adafruit_NeoPixel::Color(0, 0, 10);
-auto white = Adafruit_NeoPixel::Color(100, 100, 100);
+CRGB ledColors[] = {
+	CRGB(255, 255, 255),
+	CRGB(255, 255, 255),
+	CRGB(255, 255, 255)
+};
 
-uint32_t colors[] = {R, G, B};
+bool firstRun = true;
 
 // RFID reader config
 // RFID on/off period in millis
 #define RFID_PERID 220
 const int numRfidReaders = 2;
 // PINs to controls RFID reader, connects to Reed relays
-const int rfidPins[numRfidReaders] = {32, 33};
+const int rfidPins[numRfidReaders] = {5, 16};
 
 
 int rfidOnIdx = 0;
@@ -36,47 +39,80 @@ unsigned long prevToggleTs = 0;
 void setup()
 {
 	Serial.begin(115200);
-	Serial.setTimeout(20);
-	for (auto led : leds)
-	{
-		led->begin();
-		led->clear();
-	}
+	Serial.setTimeout(10);
+	delay(2000);
+
 	for (auto pin: rfidPins){
 		pinMode(pin, OUTPUT);
 		digitalWrite(pin, 0);
 	}
 
+	FastLED.addLeds<SK6812, PIN_LED_1, RGB>(led1, NUMPIXELS); // GRB ordering is typical
+	FastLED.addLeds<SK6812, PIN_LED_2, RGB>(led2, NUMPIXELS); // GRB ordering is typical
+	FastLED.addLeds<SK6812, PIN_LED_3, RGB>(led3, NUMPIXELS); // GRB ordering is typical
+
+	leds.push_back(led1);
+	leds.push_back(led2);
+	leds.push_back(led3);
 }
 
-int brightness[] = {0, 0, 0};
-bool on[] = {true, true, true};
 
-void ledLoop(int idx)
+void processSerialMessages()
 {
-	leds[idx]->fill(white);
-	leds[idx]->setBrightness(brightness[idx]);
-	leds[idx]->show();
-	if (on[idx])
+	if (Serial.available() > 0)
 	{
-		brightness[idx]++;
-		if (brightness[idx] > 255)
+		String msg = Serial.readStringUntil('\n');
+		Serial.println("Received " + msg);
+
+		if (msg.startsWith("on:"))
 		{
-			brightness[idx] = 255;
+			auto idx = atoi(msg.substring(3, 4).c_str());
+			auto r = atoi(msg.substring(5, 8).c_str());
+			auto g = atoi(msg.substring(9, 12).c_str());
+			auto b = atoi(msg.substring(13, 16).c_str());
+			auto tempBrightness = atoi(msg.substring(17, 20).c_str());
+			float speed = atof(msg.substring(21, 22).c_str());
+			updateLedSpeed[idx] = speed * 0.1;
+			ledColors[idx] = CRGB(r, g, b);
+			Serial.println("LED Color " + String(r) + "," + String(g) + "," + String(b));
+			Serial.println("Speed " + String(speed));
+			Serial.println("Brightness " + String(tempBrightness));
+			finalBrightnessValue[idx] = tempBrightness;
 		}
 	}
-	else
-	{
-		brightness[idx]--;
-		if (brightness[idx] <= 0)
+}
+
+void updateLed(int idx)
+{
+	float b = currentBrightnessValue[idx];
+	if ( b != finalBrightnessValue[idx] ) {
+		for (int i = 0; i < NUMPIXELS; i++)
 		{
-			brightness[idx] = 0;
+			leds[idx][i] = ledColors[idx];
+			leds[idx][i].r = dim8_video(b);
+			leds[idx][i].g = dim8_video(b);
+			leds[idx][i].b = dim8_video(b);
 		}
+		if ( finalBrightnessValue[idx] > b ) {
+			b += updateLedSpeed[idx];
+			if ( b >= finalBrightnessValue[idx] ) {
+				b = finalBrightnessValue[idx];
+			}
+			currentBrightnessValue[idx] = b;
+		} else if ( finalBrightnessValue[idx] < b ) {
+			b -= updateLedSpeed[idx];
+			if ( b <= finalBrightnessValue[idx] ) {
+				b = finalBrightnessValue[idx];
+			}
+			currentBrightnessValue[idx] = b;
+		}
+		// Serial.println("Animate LED " + String(idx) + ", " + String(b));
 	}
 }
 
 void toggleRFID()
 {
+
 	auto now = millis();
 	if (prevToggleTs == 0 || (now - prevToggleTs) > RFID_PERID)
 	{
@@ -89,42 +125,16 @@ void toggleRFID()
 	}
 }
 
-bool firstRun = true;
+
 void loop()
 {
-	// Serial.println(millis());
-	if (firstRun)
-	{
-		if (millis() > 2000)
-		{
-			on[0] = false;
-			on[1] = false;
-			on[2] = false;
-			firstRun = false;
-		}
-	}
-
 	toggleRFID();
+	processSerialMessages();
 
-	ledLoop(0);
-	ledLoop(1);
-	ledLoop(2);
-	if (Serial.available() > 0)
-	{
-		auto msg = Serial.readString();
-		Serial.println("Received " + msg);
+	updateLed(0);
+	updateLed(1);
 
-		if (msg.startsWith("on:"))
-		{
-			auto idx = atoi(msg.substring(3, 4).c_str());
-			Serial.println(String(idx) + " on");
-			on[idx] = true;
-		}
-		else if (msg.startsWith("off:"))
-		{
-			auto idx = atoi(msg.substring(4, 5).c_str());
-			Serial.println(String(idx) + " off");
-			on[idx] = false;
-		}
-	}
+
+	FastLED.show();
+	
 }
